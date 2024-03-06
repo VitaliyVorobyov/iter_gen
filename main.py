2ca0d5d85380895be77bf8cd441f9f16ea71a03a
VitaliyVorobyov <123983999+VitaliyVorobyov@users.noreply.github.com>
2024-03-06 20:49:05 +0300

    init commit

---
+++ main.py
@@ -0,0 +1,70 @@
+import types
+
+
+class FlatIterator:
+    def __init__(self, data):
+        self.data = data
+        self.outer_index = 0
+        self.inner_index = 0
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        while self.outer_index < len(self.data):
+            if self.inner_index < len(self.data[self.outer_index]):
+                result = self.data[self.outer_index][self.inner_index]
+                self.inner_index += 1
+                return result
+            else:
+                self.inner_index = 0
+                self.outer_index += 1
+        raise StopIteration
+
+
+def flat_generator(nested_list):
+    for sublist in nested_list:
+        for item in sublist:
+            yield item
+
+
+def test_1():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            FlatIterator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+        assert flat_iterator_item == check_item
+
+    assert list(FlatIterator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+
+def test_2():
+
+    list_of_lists_1 = [
+        ['a', 'b', 'c'],
+        ['d', 'e', 'f', 'h', False],
+        [1, 2, None]
+    ]
+
+    for flat_iterator_item, check_item in zip(
+            flat_generator(list_of_lists_1),
+            ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+    ):
+
+        assert flat_iterator_item == check_item
+
+    assert list(flat_generator(list_of_lists_1)) == ['a', 'b', 'c', 'd', 'e', 'f', 'h', False, 1, 2, None]
+
+    assert isinstance(flat_generator(list_of_lists_1), types.GeneratorType)
+
+
+if __name__ == '__main__':
+    test_1()
+    test_2()
